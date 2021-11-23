b9b03c04acdf276405d492a5ed8f181c18e672f3
Kam Layne <KLayne@dsm.net>
2021-11-23 12:42:58 -0500

    added check for running as admin

--- userProfileBR.ps1
+++ userProfileBR.ps1
@@ -5,6 +5,14 @@ $scriptName = $myInvocation.myCommand.name
 $path = get-location
 $timestamp = get-date -format "yyyyMMddHHmmss"
 $transcript = "$path\$timestamp-$scriptName.txt"
+$runAsAdministrator = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (!($runAsAdministrator)) {
+  write-host "You must invoke this script from an elevated PowerShell console" -f yellow
+  write-host "Launch 'Windows PowerShell (Admin)' and paste the following command into the console:`n`npowershell -executionPolicy bypass -file `"$path\$scriptName`"`n"
+  pause
+  exit
+}
 
 start-transcript -path $transcript
 write-host "`n$gitHub`n >$scriptName`n"
