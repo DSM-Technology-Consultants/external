b05a8d80d8e5e052a3b99fda6857849a7eb93f6f
Kam <kamaran.layne@gmail.com>
2022-06-02 13:07:42 -0400

    spring cleaning

--- webrootChromeBug.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set "outfile=%winDir%\Temp\wsasme.exe"
-powershell -executionPolicy bypass -windowStyle hidden -command "invoke-webRequest -uri 'https://download.webroot.com/9.0.31.79/wsasme.exe' -outFile '%outfile%'"
-start "%outfile%" /silent
